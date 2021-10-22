c8d737a2c198c5519b4a2f396840fbf69b476a55
Rajneesh Kumar <prajneesh220@gmail.com>
2021-10-22 17:18:09 +0530

    init changes

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { View, Text } from 'react-native'
 export default function App() {
     return (
         <View>
-            <Text>how are you</Text>
+            <Text>how are you Rajneesh This is github testing</Text>
         </View>
     )
 }
