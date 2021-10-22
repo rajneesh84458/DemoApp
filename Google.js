1ceb21d236a3cf069a7dc4fa130329fafc4170d9
Rajneesh Kumar <prajneesh220@gmail.com>
2021-10-22 17:40:41 +0530

    some changes

--- Google.js
+++ Google.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Google = () => {
     return (
         <div>
-           <h1>This is a React js file.</h1> 
+           <h1>This is a React js and   Google file.</h1> 
         </div>
     )
 }
